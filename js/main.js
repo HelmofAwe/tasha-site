2bc62f51c351b4eb51da2102556f768d6e589da2
Tasha DiBiagio <tashadibiagio@gmail.com>
2016-06-13 20:12:47 -0400

    added new barbie before image as png, added image overlay to thumbs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
-    
-    $("#flip").click(function(){
-        $("#panel").slideToggle("slow");
-    });
-    
-    
+var amountScrolled = 50;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn(150);
+	} else {
+		$('a.back-to-top').fadeOut(150);
+	}
+});
+
+$('a.back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
 });
